11fc995be8f888d521a09541151a7949d5419bfc
Noah <nmedoff123@gmail.com>
2016-03-01 13:37:38 -0800

    JS-Task28-Render-StorageSection-Library created a function that checks if the userGame is true

--- scripts/app.js
+++ scripts/app.js
@@ -92,12 +92,20 @@ function renderGameItem (gameItemIndex){
   gameItemContainer.id = gameItemIndex.gameID;
   gameItemContainer.setAttribute('class','game-item');
   gameItemContainer.innerHTML = gameItemContent;
-
+  return gameItemContainer;
 }
 
 // check if userGame = true and inside call render function
-
-// check if userGame = false and inside call render function
+function userGameTrueCheck(){
+  for(var i = 0; i < listOfGames.length; i++){
+    var renderedGame = renderGameItem(listOfGames[i]);
+    if(listOfGames[i].userGame === true){
+      containerUserGames.appendChild(renderedGame);
+    } else {
+      containerBuiltInGameLibrary.appendChild(renderedGame);
+    }
+  }
+}
 
 //event listener on home page for getRandomGameArrayElement
 function renderRandomGame (){
@@ -106,3 +114,5 @@ function renderRandomGame (){
 
 var randomGameButton = document.getElementById('js-generate-random-game-button');
 randomGameButton.addEventListener('click', renderRandomGame);
+
+userGameTrueCheck();
