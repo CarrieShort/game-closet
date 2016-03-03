90ad4fba44a20cda2e79e7ad45b2d079e436415e
recklessmoxie <shellybarkley@ymail.com>
2016-03-02 16:46:57 -0800

    added text to description to test css, tinkering with boxes and layout

--- scripts/storage.js
+++ scripts/storage.js
@@ -3,7 +3,7 @@
 'use strict';
 //Array keys: gamename, minPlayers, maxPlayers, minAge, intoxicated, time, rating, description,
 var dataApplesToApples = ['Apples to Apples', 4, 10, 12, 'false', 30, 6,
-  'description'
+  'description text additions to text the css that i am creating. because it needs to be here'
 ];
 var dataBalderdash = ['Balderdash', 2, 6, 12, 'true', 30, 6, 'description'];
 var dataBattleship = ['Battleship', 2, 2, 8, 'true', 30, 4.5, 'description'];
